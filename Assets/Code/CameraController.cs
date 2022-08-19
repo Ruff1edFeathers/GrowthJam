3144d53e456febbbe9b0c0d897f68e3afc6b64f2
RuffledFeathers <105522716+Ruff1edFeathers@users.noreply.github.com>
2022-08-19 22:49:59 +0100

    Initial Commit

---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform m_Target;
+    public float     m_RotationSpeed;
+
+}
